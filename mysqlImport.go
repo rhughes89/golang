0b7997985fd66eb1a87776a92cb77527053d0187
Jose M Robles <jr@jrobles.net>
2014-05-01 00:00:35 -0400

    Added command line argument to specify the input csv file

--- mysqlImport.go
+++ mysqlImport.go
@@ -11,8 +11,11 @@ import (
 
 func main () {
 
+	// CLI ARGUMENTS
+	csvFile := os.Args[1]
+
 	// OPEN THE CSV FILE
-	file, err := os.Open("wm.csv")
+	file, err := os.Open(csvFile)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -20,7 +23,7 @@ func main () {
 	defer file.Close()
 
 	// CONNECT TO THE DATABASE
-	con, err := sql.Open("mysql", "root:@/fct")
+	con, err := sql.Open("mysql", "root:SlackeR95@@/test")
 	if err != nil {
 		fmt.Println("MySQL connect ERROR: ", err)
 		return		
@@ -44,7 +47,7 @@ func main () {
 		lineCount += 1
 
 		// INSERT INTO THE DATABASE
-		_, err = con.Exec("insert into oof (ITEM_ID,WM_DEPT_NUM,WM_ITEM_NUM,WM_HOST_DESCRIPTION,UPC,PRIMARY_SHELF_ID,IS_BASE_ITEM,VARIANT_ITEMS_NUM,BASE_ITEM_ID) values (?,?,?,?,?,?,?,?,?)",record[0],record[1],record[2],record[3],record[4],record[5],record[6],record[7],record[8])
+		_, err = con.Exec("insert into testTable (ITEM_ID,WM_DEPT_NUM,WM_ITEM_NUM,WM_HOST_DESCRIPTION,UPC,PRIMARY_SHELF_ID,IS_BASE_ITEM,VARIANT_ITEMS_NUM,BASE_ITEM_ID) values (?,?,?,?,?,?,?,?,?)",record[0],record[1],record[2],record[3],record[4],record[5],record[6],record[7],record[8])
 		if err != nil {
 			fmt.Println("ERROR: ",err)		
 		}
