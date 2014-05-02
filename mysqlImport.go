ccbe3bf1e564c0530167d7fc638bb86737ea1b72
Jose M. Robles <jose.robles@ksckreate.com>
2014-05-02 16:14:22 -0400

    can now specify the num of workers as a param

--- mysqlImport.go
+++ mysqlImport.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"strconv"
 	"os"
 	"database/sql"
 		_ "github.com/go-sql-driver/mysql"
@@ -12,40 +13,35 @@ import (
 func worker(handle *sql.DB,ch chan []string){
 	for m := range ch{
 
-		stmnt, err := handle.Prepare("insert into testTable (ITEM_ID,WM_DEPT_NUM,WM_ITEM_NUM,WM_HOST_DESCRIPTION,UPC,PRIMARY_SHELF_ID,IS_BASE_ITEM,VARIANT_ITEMS_NUM,BASE_ITEM_ID) values (?,?,?,?,?,?,?,?,?)")
+	_, err := handle.Exec("insert into test (ITEM_ID,WM_DEPT_NUM,WM_ITEM_NUM,WM_HOST_DESCRIPTION,UPC,PRIMARY_SHELF_ID,IS_BASE_ITEM,VARIANT_ITEMS_NUM,BASE_ITEM_ID) values (?,?,?,?,?,?,?,?,?)",m[0],m[1],m[2],m[3],m[4],m[5],m[6],m[7],m[8])
 		if err != nil {
-			fmt.Println("ERROR: Cannot prepare the query ",err)		
+			fmt.Println("INSERT ERROR:",err)		
 		}
-
-		res, err := stmnt.Exec(m[0],m[1],m[2],m[3],m[4],m[5],m[6],m[7],m[8])
-		if err != nil {
-			fmt.Println("ERROR: Cannot execute the query")		
-		}
-		fmt.Println(res)
-
 	}
 }
-
 
 func main () {
 
 	// CLI ARGUMENTS
 	csvFile := os.Args[1]
-	//workerCount := os.Args[2]
-	workerCount := 10
+	workerCount := os.Args[2]
+	iWorker, err := strconv.Atoi(workerCount)
+	if err != nil {
+		fmt.Println(iWorker)
+	}
 
 	// OPEN THE CSV FILE
 	file, err := os.Open(csvFile)
 	if err != nil {
-		fmt.Println("CSV OPEN ERROR: ", err)
+		fmt.Println("CSV OPEN ERROR:", err)
 		return
 	}
 	defer file.Close()
 
 	// CONNECT TO THE DATABASE
-	con, err := sql.Open("mysql", "root:SlackeR95@@/test")
+	con, err := sql.Open("mysql","root:@unix(/tmp/mysql.sock)/fct?loc=Local")
 	if err != nil {
-		fmt.Println("DB CONNECT ERROR: ", err)
+		fmt.Println("DB CONNECT ERROR:", err)
 		return		
 	}
 	defer con.Close()
@@ -53,12 +49,13 @@ func main () {
 	// READ THE CSV FILE
 	reader := csv.NewReader(file)
 	reader.Comma = ','
+	lineCount := 0
 
 	// CREATE THE CHANNEL
 	records_ch := make(chan []string)
 
 	// DO WORKER
-	for i := 0; i < workerCount; i++ {
+	for i := 0; i < iWorker; i++ {
 		go worker(con,records_ch)
 	}
 
@@ -68,16 +65,13 @@ func main () {
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			fmt.Println("CSV ITERATE ERROR: ", err)
+			fmt.Println("CSV ITERATE ERROR:", err)
 			return
 		}
-/*
-		_, err = con.Exec("insert into testTable (ITEM_ID,WM_DEPT_NUM,WM_ITEM_NUM,WM_HOST_DESCRIPTION,UPC,PRIMARY_SHELF_ID,IS_BASE_ITEM,VARIANT_ITEMS_NUM,BASE_ITEM_ID) values (?,?,?,?,?,?,?,?,?)",record[0],record[1],record[2],record[3],record[4],record[5],record[6],record[7],record[8])
-		if err != nil {
-			fmt.Println("INSERT ERROR: ",err)		
-		}
-*/	
+
 		// LETS POPULATE THAT CHANNEL
 		records_ch <- record
+		lineCount ++
 	}
+	fmt.Println("PROCESSED",lineCount,"RECORDS")
 }
